ac45eb4848740b439a61a2717d5a8acab007d826
Seyi Obaweya <seyi@globalmobility.net>
2019-09-16 18:38:32 +0800

    added projectfiles

---
+++ index.js
@@ -0,0 +1,39 @@
+// //getting values from each field
+// var firstName = $("input#firstname").val();
+// var lastName  = $("input#lastname").val();
+// var userName  = $("input#username").val();
+// var password = $("input#password").val();
+// var gender = $("select#gender").val();
+// var terms = ( $('input[type="checkbox"]').prop("checked") == true) ? true: false;
+
+
+// var submit = $("button#form-submit").on("click", function(){
+//     event.preventDefault(); //prevents default action
+//     var profile = {
+//         "firstName": $("input#firstname").val(),
+//         "lastName": $("input#lastname").val(),
+//         "userName": $("input#username").val(),
+//         "password": $("input#password").val(),
+//         "gender": $("select#gender").val(),
+//         "terms": terms
+//     }
+// })
+
+// // const form = document.getElementsByClassName("main-form")[0]
+
+// // const data = getFormDataAsJSON(form);
+
+// // const handleFormSubmit = event => {
+
+// //     // Stop the form from submitting since we’re handling that with AJAX.
+// //     event.preventDefault();
+  
+// //     // TODO: Call our function to get the form data.
+// //     const data = {};
+
+  
+// //     // Use `JSON.stringify()` to make the output valid, human-readable JSON.
+// //     dataContainer.textContent = JSON.stringify(data, null, "  ");
+  
+// //     // ...this is where we’d actually do something with the form data...
+// //   };
