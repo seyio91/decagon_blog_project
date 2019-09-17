7e4e00ae4aa5de971cb2629bd33e82c6f2ee1dc0
Seyi Obaweya <seyi@globalmobility.net>
2019-09-18 00:12:46 +0800

    displayed comments in body

--- views.js
+++ views.js
@@ -15,14 +15,39 @@ $.get(url, function(data){
     console.log(data);
     $(document).prop('title', data.title);
         $("#blogview").append(`
+        <article>
         <div id="postbody">
             <div id="post_header">
-                <p>${data.title}</p>
+                    <h2>${data.title}</h2>
+                </div>
+         
+            <div class="row">
+                <div class="group1 col-sm-6 col-md-6">
+                        <i class="fas fa-folder-open"></i>  <a href="#">Signs</a>
+                        <i class="fas fa-bookmark"></i> <a href="#">Aries</a>,
+                        <a href="#">Fire</a>, <a href="#">Mars</a>
+                </div>
+                <div class="group2 col-sm-6 col-md-6">
+                        <i class="fas fa-user-edit"></i> <a href="singlepost.html#comments">20 Comments</a>
+                        <i class="far fa-clock"></i> August 24, 2013 9:00 PM
+                </div>
             </div>
-            <div id="post_body">
-                <p>${data.blog_main}</p>
+         
+            <hr>
+         
+            <img src="./images/ThumperDC.jpg" class="img-fluid">
+         
+            <br />
+         
+            <p class="lead">
+                <div id="post_body">
+                    <p>${data.blog_main}</p>
+                </div>
+            </p>
+         
+            <hr>
             </div>
-        </div>
+        </article>
         `)
         
 });
@@ -84,16 +109,18 @@ const cUrl = "http://localhost:3000/comments/?blog_id=" + id
 $.get(cUrl, function(data){
     console.log(data);
     for (let i = 0; i < data.length; i++){
-        $("#comment_list").append(`<li>
-            <div id="comment_head">
-                <div id="post_header">
-                    <p>${data[i].cName}</p>
-                </div>
-                <div id="comment_body">
-                    <p>${data[i].cBody}...</p>
-                </div>
+        $("#comment_list").append(`
+            <li class="comment">
+            <div class="clearfix">
+                <h4 class="pull-left">${data[i].cName}</h4>
+                <p class="pull-right">9:41 PM on August 24, 2013</p>
             </div>
-            </li>
+            <p>
+                <em>${data[i].cBody}</em>
+            </p>
+        </li>
+
+
             `)
     }    
 });
